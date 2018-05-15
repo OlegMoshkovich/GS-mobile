eac89986b35230420c467eccdc57a81506367bbd
vomc <vomc8one@gmail.com>
2018-05-15 18:28:22 -0400

    formatting ava menu style

--- src/styles/avabottommenu.js
+++ src/styles/avabottommenu.js
@@ -7,16 +7,16 @@ import {
 var styles = StyleSheet.create({
     // icons next to ava
     navMenu: {
-        flex:1,
+        flex: 1,
         flexDirection: 'row',
         position: 'absolute',
-        left:20,
+        left: 20,
     },
 
     navButton: {
         margin: 5,
     },
-    
+
     navImage: {
         height: 35,
         width: 35,
@@ -31,27 +31,28 @@ var styles = StyleSheet.create({
     },
 
     avaImage: {
-        width: 100, height: 100,
+        width: 100,
+        height: 100,
     },
 
     // revised ava menu with new blue gradient
     avaImage: {
-        height:354/3,
-        width:285/3,
+        height: 354 / 3,
+        width: 285 / 3,
     },
 
     gradientStyle: {
         height: 87,
-            zIndex: 0,
-            position:'absolute',
+        zIndex: 0,
+        position: 'absolute',
     },
 
     navStyle: {
-        
-            flex:1,
-            zIndex: 2,
-            flexDirection: 'row',
-            justifyContent: 'space-around',
+
+        flex: 1,
+        zIndex: 2,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
     },
 
 
