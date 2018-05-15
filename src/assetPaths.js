ad6f8f3ae1c987a1d4164df97a48069ee44160d7
vomc <vomc8one@gmail.com>
2018-05-15 14:52:30 -0400

    formatting

--- src/assetPaths.js
+++ src/assetPaths.js
@@ -1,34 +1,22 @@
 /*
 
-
-
-
 organizing asset paths here 
-
-
 
 */
 
 
 let paths = {};
 
-
-
 // bottom menu with ava and icons
 paths.bottomMenu = {
 
-    avaFace: require('../assets/bottomMenu/ava/Nav_Avatar_Face_Animations.png'),
+    avaFace:        require('../assets/bottomMenu/ava/Nav_Avatar_Face_Animations.png'),
 
-    // icons
-    exploreIcon: require('../assets/bottomMenu/icons/Explore-icon.png'),
-    mapIcon: 
-    require('../assets/bottomMenu/icons/Map-icon.png'),
-    communityIcon: 
-    require('../assets/bottomMenu/icons/Community-icon.png'),
-    calendarIcon: 
-    require('../assets/bottomMenu/icons/Calendar-icon.png'),
-    shopIcon:
-    require('../assets/bottomMenu/icons/Shop-icon.png')
+    exploreIcon:    require('../assets/bottomMenu/icons/Explore-icon.png'),
+    mapIcon:        require('../assets/bottomMenu/icons/Map-icon.png'),
+    communityIcon:  require('../assets/bottomMenu/icons/Community-icon.png'),
+    calendarIcon:   require('../assets/bottomMenu/icons/Calendar-icon.png'),
+    shopIcon:       require('../assets/bottomMenu/icons/Shop-icon.png')
 
 };
 export default paths;
