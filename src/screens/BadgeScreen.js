0c68d59a54c4862f2b8953320b4cd0ff30702dec
vomc <vomc8one@gmail.com>
2018-05-17 13:45:48 -0400

    progress #16

---
+++ src/screens/BadgeScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Dimensions from 'Dimensions';
+import { Modal, StyleSheet, Text, View, TouchableOpacity,Image, TouchableHighlight,ScrollView,Toggle, Alert} from 'react-native';
+import {StackNavigator,TabNavigator, TabBarBottom} from 'react-navigation';
+import { LinearGradient } from "expo";
+import {Card, Button,Icon} from 'react-native-elements';
+const {width, height} = Dimensions.get('window');
+
+
+import AvaBottomMenu from '../components/AvaBottomMenu.js';
+import TopMenu from '../components/TopMenu';
+import assetPaths from '../assetPaths';
+
+class BadgeScreen extends React.Component {
+ constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+        <View>
+            <TopMenu menuTitle="#endyellow" iconPath={assetPaths.topMenu.exploreIcon} />
+            <View><Text>badges</Text></View>
+            <AvaBottomMenu navigation={this.props.navigation}/> 
+        </View>
+
+      
+    );
+  }
+}
+
+export default BadgeScreen;
---
+++ src/screens/JourneyScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Dimensions from 'Dimensions';
+import { Modal, StyleSheet, Text, View, TouchableOpacity,Image, TouchableHighlight,ScrollView,Toggle, Alert} from 'react-native';
+import {StackNavigator,TabNavigator, TabBarBottom} from 'react-navigation';
+import { LinearGradient } from "expo";
+import {Card, Button,Icon} from 'react-native-elements';
+const {width, height} = Dimensions.get('window');
+
+
+import AvaBottomMenu from '../components/AvaBottomMenu.js';
+import TopMenu from '../components/TopMenu';
+import assetPaths from '../assetPaths';
+
+class JourneyScreen extends React.Component {
+ constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+        <View>
+            <TopMenu menuTitle="#endyellow" iconPath={assetPaths.topMenu.exploreIcon} />
+            <View><Text>journey</Text></View>
+            <AvaBottomMenu navigation={this.props.navigation}/> 
+        </View>
+
+      
+    );
+  }
+}
+
+export default JourneyScreen;
