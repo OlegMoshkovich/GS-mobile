cbea246bccfe67eb13d5a126a795b9c20cf84079
vomc <vomc8one@gmail.com>
2018-06-06 18:06:31 -0400

    nav

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -37,6 +37,7 @@ class TopMenu extends Component {
 
             case 'Journey':
             case 'Award':
+            
 
                 this.props.navigation.navigate('Dashboard');
                 break;
