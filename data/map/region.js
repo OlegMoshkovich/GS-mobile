47c344f744847350052364bac56c9c1716e08e43
vomc <vomc8one@gmail.com>
2018-06-18 16:58:19 -0400

    cleanup for #140

--- data/map/region.js
+++ data/map/region.js
@@ -1,12 +1,8 @@
 let region = {
-    
-
-
-  
   latitude: 37.7935215,
-    longitude: -122.4626465,
-    latitudeDelta: 0.15,
-    longitudeDelta: 0.15,
-  };
+  longitude: -122.4626465,
+  latitudeDelta: 0.15,
+  longitudeDelta: 0.15,
+};
 
 export default region;
