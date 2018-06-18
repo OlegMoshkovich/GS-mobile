1490a3e179d524f07e6453f937c397f2173ef4c9
vomc <vomc8one@gmail.com>
2018-06-18 17:46:40 -0400

    refactor #140

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,87 +1,51 @@
 import React, {Component} from 'react';
 import Dimensions from 'Dimensions';
-import { StyleSheet, Text, View, TouchableOpacity, Image, Animated, Modal} from 'react-native';
-const {width, height} = Dimensions.get('window');
-// global style
 import s from '../styles/topmenu';
-// asset path locations
 import assetPaths from '../assetPaths'
+import { Text, View, TouchableOpacity, Image, Modal} from 'react-native';
+const {width, height} = Dimensions.get('window');
 
 class TopMenu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            badgesShowing: false,
-        }
-    }
-
-    updateData() {
-        // to be implemented
+        this.state = { badgesShowing: false, }
     }
 
     navigateBack() {
-        console.log("navigation routeName:::",this.props.navigation.state.routeName);
-
-        // console.log(this.props.navigation);
         switch (this.props.navigation.state.routeName) {
-            case 'Explore':
-                this.setState({badgesShowing: true});
-                break;
+            case 'Explore': this.setState({badgesShowing: true}); break;
             case 'Journey':
             case 'Award':
             case 'Badges':
-                this.props.navigation.navigate('Dashboard');
-                break;
+                this.props.navigation.navigate('Dashboard'); break;
             case 'Chat':
             case 'Events':
             case 'Calendar':
             case 'Map':
-                this.props.navigation.navigate('ConnectDashboard');
-                break;
-            case 'ShopModal':
-                this.props.navigation.goBack();
-                break;
-            case 'SystemModal':
-                this.props.navigation.goBack();
-                break;
-            default:
-                console.log("no navigation yet");
-                break;
+                this.props.navigation.navigate('ConnectDashboard'); break;
+            case 'ShopModal': this.props.navigation.goBack(); break;
+            case 'SystemModal': this.props.navigation.goBack(); break;
+            default: console.log("no navigation yet"); break;
         }
     }
 
-
-
     render() {
-        //console.log(this.props);
-        return (
-        <View style ={s.container}>
-
+        return (<View style ={s.container}>
             <Text style ={s.menuText}>{this.props.menuTitle}</Text>
-
-            <TouchableOpacity style ={s.menuNavButton}
-              onPress={() => this.navigateBack()}>
-
-              <Image style={s.menuNavButtonImage} source={this.props.iconPath} />
-            </TouchableOpacity>
-
-
+                <TouchableOpacity style ={s.menuNavButton}
+                    onPress={() => this.navigateBack()}>
+                    <Image style={s.menuNavButtonImage} source={this.props.iconPath} />
+                </TouchableOpacity>
+            
             {this.state.badgesShowing ?
-
-                  <Modal animationType="fade" transparent={true} visible={this.state.badgesShowing}
-                          onRequestClose={() => console.log("modal closed")}>
-
-                      <TouchableOpacity onPress={() => this.setState({badgesShowing: false})}>
-                          <Image key={'foobar123'} style={{width: width, height: height}} source={assetPaths.staticScreens.badgesOverview} />
-                      </TouchableOpacity>
-                  </Modal>
-
-
-            : null}
-
-        </View>
-        );
+                <Modal  animationType="fade" transparent={true} visible={this.state.badgesShowing}
+                        onRequestClose={() => console.log("modal closed")}>
+                    <TouchableOpacity onPress={() => this.setState({badgesShowing: false})}>
+                        <Image key={'foobar123'} style={{width: width, height: height}} source={assetPaths.staticScreens.badgesOverview} />
+                    </TouchableOpacity>
+                </Modal> : null}
+            </View>);
     }
 }
 
-export default TopMenu;
+export default TopMenu
