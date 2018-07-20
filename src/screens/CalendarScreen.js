fc23060186c3af15b9b54211c85c9495c4c8acc9
vomc <vomc8one@gmail.com>
2018-07-20 12:20:35 -0400

    revert calendar for #23

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Dimensions from 'Dimensions';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity,Image, TouchableHighlight,ScrollView,Toggle, Alert} from 'react-native';
+import {StackNavigator,TabNavigator, TabBarBottom} from 'react-navigation';
 import { LinearGradient } from "expo";
+import {Card, Button,Icon} from 'react-native-elements';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 const color = '#FFF2AD';
 
 import TopMenu from '../components/TopMenu';
+import NavMenu from '../components/NavMenu';
 import AvaBottomMenu from '../components/AvaBottomMenu';
 import assetPaths from '../assetPaths';
 
@@ -12,6 +16,7 @@ import assetPaths from '../assetPaths';
 import s from '../styles/calendarscreen';
 
 const {width, height} = Dimensions.get('window');
+
 
 class CalendarScreen extends React.Component {
   constructor(props) {
@@ -23,19 +28,40 @@ class CalendarScreen extends React.Component {
   }
 
   render() {
-    return (<LinearGradient
-      colors={[color, color]} style={{ height: height, width:width}}>
-      <TopMenu navigation={this.props.navigation} menuTitle="when" iconPath={assetPaths.topMenu.connectIcon} />
-        <View style = {{ flex:4 }}>
-          <Agenda
-            items={this.state.items}
-            loadItemsForMonth={this.loadItems.bind(this)}
-            selected={'2018-04-18'}
-            renderItem={this.renderItem.bind(this)}
-            renderEmptyDate={this.renderEmptyDate.bind(this)}
-            rowHasChanged={this.rowHasChanged.bind(this)}
-            theme={{
+    return (
+      <LinearGradient
+      colors={[color, color]}
+        style={{ height: height, width:width}}>
+        <TopMenu navigation={this.props.navigation} menuTitle="when" iconPath={assetPaths.topMenu.connectIcon} />
+
+
+
+    <View style = {{
+      flex:4
+    }}>
+
+    <Agenda
+      items={this.state.items}
+      loadItemsForMonth={this.loadItems.bind(this)}
+      selected={'2018-04-18'}
+      renderItem={this.renderItem.bind(this)}
+      renderEmptyDate={this.renderEmptyDate.bind(this)}
+      rowHasChanged={this.rowHasChanged.bind(this)}
+      // markingType={'period'}
+      // markedDates={{
+      //    '2017-05-08': {textColor: '#666'},
+      //    '2017-05-09': {textColor: '#666'},
+      //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
+      //    '2017-05-21': {startingDay: true, color: 'blue'},
+      //    '2017-05-22': {endingDay: true, color: 'gray'},
+      //    '2017-05-24': {startingDay: true, color: 'gray'},
+      //    '2017-05-25': {color: 'gray'},
+      //    '2017-05-26': {endingDay: true, color: 'gray'}}}
+      // monthFormat={'yyyy'}
+      // theme={{calendarBackground: '#F9C025', agendaKnobColor: 'darkgrey',selectedDayBackgroundColor: '#F9C025', todayTextColor: 'red',  agendaTodayColor: '#F9C025',}}
+      theme={{
                 calendarBackground: '#F9C025',
+
                 textSectionTitleColor: 'white',
                 dayTextColor: 'white',
                 todayTextColor: 'white',
@@ -43,13 +69,25 @@ class CalendarScreen extends React.Component {
                 monthTextColor: 'white',
                 selectedDayBackgroundColor: '#333248',
                 arrowColor: 'white',
+                // textDisabledColor: 'red',
                 'stylesheet.calendar.header': {
-                  week: { marginTop: 5, flexDirection: 'row', justifyContent: 'space-between' }
+                  week: {
+                    marginTop: 5,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between'
+                  }
                 }
-               }} />
-        </View>
-        <AvaBottomMenu currentSection={'connect'} contextIcon={true} navigation={this.props.navigation}/>
-      </LinearGradient>);
+               }}
+
+      //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+    />
+    </View>
+
+
+      <AvaBottomMenu currentSection={'connect'} contextIcon={true} navigation={this.props.navigation}/>
+
+      </LinearGradient>
+   );
  }
 
  loadItems(day) {
@@ -60,22 +98,29 @@ class CalendarScreen extends React.Component {
        if (!this.state.items[strTime]) {
          this.state.items[strTime] = [];
          const numItems = Math.floor(Math.random() * 5);
-        
+
          for (let j = 0; j < numItems; j++) {
            this.state.items[strTime].push({
-             name: 'Events:', height: Math.max(50, Math.floor(Math.random() * 150))
+             name: 'Events:',
+             height: Math.max(50, Math.floor(Math.random() * 150))
            });
          }
        }
      }
+     //console.log(this.state.items);
      const newItems = {};
      Object.keys(this.state.items).forEach(key => {newItems[key] = this.state.items[key];});
-     this.setState({ items: newItems });
-    }, 1000);
-  }
+     this.setState({
+       items: newItems
+     });
+   }, 1000);
+   // console.log(`Load Items for ${day.year}-${day.month}`);
+ }
 
  renderItem(item) {
-   return (<View style={[s.item, {height: item.height}]}><Text>{item.name}</Text></View>);
+   return (
+     <View style={[s.item, {height: item.height}]}><Text>{item.name}</Text></View>
+   );
  }
 
  renderEmptyDate() {
@@ -94,4 +139,4 @@ class CalendarScreen extends React.Component {
  }
 }
 
-export default CalendarScreen
+export default CalendarScreen;
