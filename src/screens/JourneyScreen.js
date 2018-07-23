52f6ae1b59595894f8be74593936182c883f0e45
vomc <vomc8one@gmail.com>
2018-07-23 19:14:18 -0400

    cleanup journey #20

--- src/screens/JourneyScreen.js
+++ src/screens/JourneyScreen.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import Dimensions from 'Dimensions';
-import { Modal, StyleSheet, Text, View, TouchableOpacity,TouchableWithoutFeedback,Image, TouchableHighlight,ScrollView,Toggle, Alert} from 'react-native';
-import {StackNavigator,TabNavigator, TabBarBottom} from 'react-navigation';
+import { View, Image,ScrollView} from 'react-native';
 import { LinearGradient } from "expo";
-import {Card, Button,Icon} from 'react-native-elements';
-const {width, height} = Dimensions.get('window');
 import Carousel from 'react-native-snap-carousel'; // 3.4.0
 
 import AvaBottomMenu from '../components/AvaBottomMenu.js';
 import TopMenu from '../components/TopMenu';
-import NavMenu from '../components/NavMenu';
+
 import assetPaths from '../assetPaths';
 import s from '../styles/journeyscreen';
-import articles from '../../data/articles/articleContent.js';
 
-const SliderWidth = Dimensions.get('screen').width;
+const {width, height} = Dimensions.get('window');
 const ItemWidth = 300.0;
 const ItemHeight = 600.0;
-
 const color = '#BACDFF'
 
 
@@ -35,33 +30,31 @@ class JourneyScreen extends React.Component {
     this._renderItem = this._renderItem.bind(this)
   }
 
-    _renderItem({ item }) {
-        return (
-          <View style={{
-            width: ItemWidth,
-            height: ItemHeight,
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: 'transparent'
-          }}>
-            <ScrollView showsVerticalScrollIndicator = {false}>
-              <View style = {{paddingBottom:50}}>
-              <Image style={{alignSelf:'center', width:786/3,height:1041/3}} resizeMode="cover" source={item.icon} />
-              </View>
-              <View style = {{paddingBottom:200}}>
-              <Image style={{alignSelf:'center', width:894/3,height:7344/3}} resizeMode="cover" source={item.image} />
-              </View>
-            </ScrollView>
+  _renderItem({ item }) {
+    return (
+      <View style={{
+        width: ItemWidth,
+        height: ItemHeight,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'transparent'
+      }}>
+        <ScrollView showsVerticalScrollIndicator = {false}>
+          <View style = {{paddingBottom:50}}>
+            <Image style={{alignSelf:'center', width:786/3,height:1041/3}} resizeMode="cover" source={item.icon} />
           </View>
-        );
-      }
+          <View style = {{paddingBottom:200}}>
+            <Image style={{alignSelf:'center', width:894/3,height:7344/3}} resizeMode="cover" source={item.image} />
+          </View>
+        </ScrollView>
+      </View>);
+  }
 
   render() {
     return (
       <LinearGradient colors={[color,color]} style={{ height: height, width:width }}>
         <TopMenu navigation={this.props.navigation} menuTitle="journeys" iconPath={assetPaths.topMenu.dashboardIcon} />
-
-          <View style={{ flex: 4}}>
+        <View style={{ flex: 4}}>
           <Carousel
             ref={(c) => { this._carousel = c; }}
             data={Items}
@@ -76,4 +69,4 @@ class JourneyScreen extends React.Component {
   }
 }
 
-export default JourneyScreen;
+export default JourneyScreen
