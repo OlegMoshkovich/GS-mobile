3072d0cd55ed5633ddc73cc058df2a1e9ec2d5be
vomc <vomc8one@gmail.com>
2018-07-23 17:03:06 -0400

    refactor dashboard #20

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import Dimensions from 'Dimensions';
-import { StyleSheet, Text, View, TouchableOpacity,Image, TouchableHighlight,ScrollView,Toggle, Alert } from 'react-native';
-import { StackNavigator,TabNavigator, TabBarBottom } from 'react-navigation';
+import { Text, View, TouchableOpacity,Image,ScrollView } from 'react-native';
 import { LinearGradient } from "expo";
-import { Card, Button,Icon} from 'react-native-elements';
-import { Ionicons } from '@expo/vector-icons';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
-const  { width, height} = Dimensions.get('window');
-const color = '#BACDFF'
 
 import AvaBottomMenu from '../components/AvaBottomMenu.js';
 import TopMenu from '../components/TopMenu';
-import NavMenu from '../components/NavMenu';
-import assetPaths from '../assetPaths';
 
+import assetPaths from '../assetPaths';
 import s from '../styles/dashboardscreen.js';
+
+const  { width, height} = Dimensions.get('window');
+const color = '#BACDFF'
 
 class DashboardScreen extends React.Component {
 
@@ -27,115 +23,69 @@ class DashboardScreen extends React.Component {
   };
 
   renderDashboardMenu() {
-
-    // TODO make global: also that tap icon does not suggest swiping
     return(
       <View style ={s.container}>
-           <Text style ={s.menuText}>#dashboard</Text>
-           <Image
-             style={s.menuTapHereIcon}
-             source={require('../../assets/Tap_here_icon.png')}
-           />
-          <TouchableOpacity style ={s.dashboardIconButton}
-            onPress={() => this.props.navigation.navigate({
-            routeName: 'Home', params: { transition: 'left' }}
-            )}>
-            <Image style={s.dashboardIconImage}
-              source={require('../../assets/Dashboard-icon.png')} />
-          </TouchableOpacity>
-      </View>
-    );
-  }
-  renderAvaDashboard() {
-
-    // TODO -- need to remove the touchable Opacity items here that
-    // right now just give padding
-
-    // do we want to add numbers here too for points?
-    return(
-
-      <View style={s.mainDashboard}>
-
-
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate({
-          routeName: 'Badges'}
+        <Text style ={s.menuText}>#dashboard</Text>
+        <Image style={s.menuTapHereIcon} source={require('../../assets/Tap_here_icon.png')} />
+        <TouchableOpacity style ={s.dashboardIconButton} onPress={() => this.props.navigation.navigate({
+          routeName: 'Home', params: { transition: 'left' }}
           )}>
-          <Text style = {{color:'white',bottom:10,alignSelf:'center'}}>4</Text>
-          <Image
-            style={s.progressImage}
-            source={require('../../assets/icons/Dashboard_Icon_Badges.png')} />
+          <Image style={s.dashboardIconImage} source={require('../../assets/Dashboard-icon.png')} />
         </TouchableOpacity>
-
-        <TouchableOpacity
-        onPress={() => this.props.navigation.navigate({
-        routeName: 'Journey'}
-        )}>
-        <Text style = {{color:'white',bottom:10,alignSelf:'center'}}>1</Text>
-          <Image style={s.progressImage}
-            source={require('../../assets/icons/Dashboard_Icon_Journeys.png')} />
-        </TouchableOpacity>
-        <TouchableOpacity
-        onPress={() => this.props.navigation.navigate({
-        routeName: 'Award'}
-        )}>
-        <Text style = {{color:'white',bottom:10,alignSelf:'center'}}>1</Text>
-          <Image style={s.progressImage}
-            source={require('../../assets/icons/Dashboard_Icon_Awards.png')} />
-        </TouchableOpacity>
-
-
       </View>
     );
   }
+  
+  renderAvaDashboard() {
+    return(
+      <View style={s.mainDashboard}>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate({ routeName: 'Badges'} )}>
+          <Text style = {{color:'white',bottom:10,alignSelf:'center'}}>4</Text>
+          <Image style={s.progressImage} source={require('../../assets/icons/Dashboard_Icon_Badges.png')} />
+        </TouchableOpacity>
+        
+        <TouchableOpacity onPress={() => this.props.navigation.navigate({ routeName: 'Journey'} )}>
+        <Text style = {{color:'white',bottom:10,alignSelf:'center'}}>1</Text>
+          <Image style={s.progressImage} source={require('../../assets/icons/Dashboard_Icon_Journeys.png')} />
+        </TouchableOpacity>
+        
+        <TouchableOpacity onPress={() => this.props.navigation.navigate({ routeName: 'Award'} )}>
+          <Text style = {{color:'white',bottom:10,alignSelf:'center'}}>1</Text>
+          <Image style={s.progressImage} source={require('../../assets/icons/Dashboard_Icon_Awards.png')} />
+        </TouchableOpacity>
+      </View>);
+  }
+  
   renderBadges() {
-    // TODO - not sure I understand what state.environmentSwitch is used for here...
     return(
       <View style={s.badgeContainer}>
-      <ScrollView horizontal = {true} showsHorizontalScrollIndicator = {false} style={{left:15}}>
-            <TouchableOpacity  style={s.imagePadding}>
-            <Image
-            style={s.badgeImage}
-            source={require('../../assets/icons/Badges/Icon_Badge_1.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity  style={s.imagePadding}>
-            <Image
-              style={s.badgeImage}
-              source={require('../../assets/icons/Badges/Icon_Badge_2.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity  style={s.imagePadding}>
-            <Image
-              style={s.badgeImage}
-              source={require('../../assets/icons/Badges/Icon_Badge_3.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity  style={s.imagePadding}>
-            <Image
-              style={s.badgeImage}
-              source={require('../../assets/icons/Badges/Icon_Badge_4.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity  style={s.imagePadding}>
-            <Image
-              style={s.badgeImage}
-              source={require('../../assets/icons/Badges/Icon_Badge_1.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity  style={s.imagePadding}>
-              <Image
-                style={s.badgeImage}
-                source={require('../../assets/icons/Badges/Icon_Badge_2.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity  style={s.imagePadding}>
-              <Image
-                style={s.badgeImage}
-                source={require('../../assets/icons/Badges/Icon_Badge_3.png')} />
-            </TouchableOpacity>
-            <TouchableOpacity  style={s.imagePadding}>
-              <Image
-                style={s.badgeImage}
-                source={require('../../assets/icons/Badges/Icon_Badge_4.png')} />
-            </TouchableOpacity>
+        <ScrollView horizontal = {true} showsHorizontalScrollIndicator = {false} style={{left:15}}>
+          <TouchableOpacity  style={s.imagePadding}>
+            <Image style={s.badgeImage} source={require('../../assets/icons/Badges/Icon_Badge_1.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity  style={s.imagePadding}>
+            <Image style={s.badgeImage} source={require('../../assets/icons/Badges/Icon_Badge_2.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity  style={s.imagePadding}>
+            <Image style={s.badgeImage} source={require('../../assets/icons/Badges/Icon_Badge_3.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity  style={s.imagePadding}>
+            <Image style={s.badgeImage} source={require('../../assets/icons/Badges/Icon_Badge_4.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity  style={s.imagePadding}>
+            <Image style={s.badgeImage} source={require('../../assets/icons/Badges/Icon_Badge_1.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity  style={s.imagePadding}>
+            <Image style={s.badgeImage} source={require('../../assets/icons/Badges/Icon_Badge_2.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity  style={s.imagePadding}>
+            <Image style={s.badgeImage} source={require('../../assets/icons/Badges/Icon_Badge_3.png')} />
+          </TouchableOpacity>
+          <TouchableOpacity  style={s.imagePadding}>
+            <Image style={s.badgeImage} source={require('../../assets/icons/Badges/Icon_Badge_4.png')} />
+          </TouchableOpacity>
         </ScrollView>
-        </View>
-    );
+      </View>);
   }
 
   render() {
@@ -152,4 +102,4 @@ class DashboardScreen extends React.Component {
   }
 }
 
-export default DashboardScreen;
+export default DashboardScreen
