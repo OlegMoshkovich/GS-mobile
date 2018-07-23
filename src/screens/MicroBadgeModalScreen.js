caaf748e39125ba88f19629dcd01bebafa92c81c
vomc <vomc8one@gmail.com>
2018-07-23 19:25:48 -0400

    fixing microbadge modal #20

--- src/screens/MicroBadgeModalScreen.js
+++ src/screens/MicroBadgeModalScreen.js
@@ -1,96 +1,47 @@
 import React from 'react';
+import { View, TouchableOpacity, Image, ScrollView} from 'react-native';
 import Dimensions from 'Dimensions';
-import { StyleSheet, Text, View, TouchableOpacity,Image, TouchableHighlight,ScrollView,Toggle, Alert, Animated} from 'react-native';
-import {StackNavigator,TabNavigator, TabBarBottom} from 'react-navigation';
-import {Card, Button,Icon} from 'react-native-elements';
-const {width, height} = Dimensions.get('window');
-
 
 import s from '../styles/storymodal';
 import assetPaths from '../assetPaths';
-import storyContent from '../../data/stories/stories';
 
-
+const {width, height} = Dimensions.get('window');
 
 class MicroBadgeModalScreen extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			friendRequested: false,
-			heartSolid: false,
-			starSolid: false,
-
-		};
-		//console.log("story modal screen props", this.props);
-
-
 	}
-
-	addFriend() {
-
-		if (this.state.friendRequested) {
-			this.setState({
-				friendRequested: false,
-			});
-		} else {
-			this.setState({
-				friendRequested: true,
-			});
-		}
-
-	}
-
+	
 	renderStoryPng(id) {
-
 		switch (id) {
 			case 1:
 				return (<Image style={{width: width, height: height, marginBottom: 100, top: -100}} source={require('../../assets/staticScreens/microbadges/MicroBadgeModaldScreen.png')} />);
-				break;
-
+			break;
 		}
 	}
 
-  render() {
-
+  	render() {
 		const {params} = this.props.navigation.state;
 		const id = params ? params.id: 1;
-		console.log("rendering id", id);
-
-    return (
-		<View style={s.modalContainer}>
-
-			<View style={[s.modalMenuContainer, {width: width}]}>
-				<TouchableOpacity style={s.modalButton} >
-					<Image style={s.modalIconImage} source={assetPaths.modals.shareModal.share} />
-				</TouchableOpacity>
-				<TouchableOpacity style={s.modalButton} onPress={() => this.setState({starSolid: !this.state.starSolid})}>
-
-					{ this.state.starSolid ?
-						<Image style={s.modalIconImage} source={assetPaths.modals.shareModal.starSolid} /> :
-						<Image style={s.modalIconImage} source={assetPaths.modals.shareModal.star} />
-					}
-
-				</TouchableOpacity>
-				<TouchableOpacity style={s.modalButton} onPress={() => this.setState({heartSolid: !this.state.heartSolid})}>
-
-					{ this.state.heartSolid ?
-						<Image style={s.modalIconImage} source={assetPaths.modals.shareModal.likeSolid} /> :
-						<Image style={s.modalIconImage} source={assetPaths.modals.shareModal.like} />
-					}
-
-				</TouchableOpacity>
-				<TouchableOpacity style={s.exitButton} onPress={() => this.props.navigation.goBack()} >
-					<Image style={s.exitIconImage} source={assetPaths.modals.shareModal.close}/>
-				</TouchableOpacity>
-
-		</View>
-  		<ScrollView style={s.storyContentContainer}>
-  			{ this.renderStoryPng(id)}
-		</ScrollView>
-
-
-</View>);
-  }
+    	return (
+			<View style={s.modalContainer}>
+				<View style={[s.modalMenuContainer, {width: width}]}>
+					<TouchableOpacity style={s.modalButton} >
+						<Image style={s.shareIcon} source={assetPaths.modals.badgeModal.share} />
+					</TouchableOpacity>
+					<TouchableOpacity style={s.modalButton} >
+						<Image style={s.starIcon} source={assetPaths.modals.badgeModal.star} />
+					</TouchableOpacity>
+					<TouchableOpacity style={s.modalButton} >
+						<Image style={s.heartIcon} source={assetPaths.modals.badgeModal.like} />
+					</TouchableOpacity>
+					<TouchableOpacity style={s.exitButton} onPress={() => this.props.navigation.goBack()} >
+						<Image style={s.exitIconImageBadge} source={assetPaths.modals.badgeModal.close}/>
+					</TouchableOpacity>
+				</View>
+  				<ScrollView style={s.storyContentContainer}>{ this.renderStoryPng(id)}</ScrollView>
+			</View>);
+  	}
 }
 
-export default MicroBadgeModalScreen;
+export default MicroBadgeModalScreen
