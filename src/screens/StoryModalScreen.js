95ce45c746957a74db6974109318619715f0d146
vomc <vomc8one@gmail.com>
2018-07-23 19:46:18 -0400

    cleanup #20

--- src/screens/StoryModalScreen.js
+++ src/screens/StoryModalScreen.js
@@ -1,47 +1,26 @@
 import React from 'react';
 import Dimensions from 'Dimensions';
-import { StyleSheet, Text, View, TouchableOpacity,Image, TouchableHighlight,ScrollView,Toggle, Alert, Animated} from 'react-native';
-import {StackNavigator,TabNavigator, TabBarBottom} from 'react-navigation';
-import {Card, Button,Icon} from 'react-native-elements';
-const {width, height} = Dimensions.get('window');
-
+import {View, TouchableOpacity,Image,ScrollView} from 'react-native';
+const {width} = Dimensions.get('window');
 
 import s from '../styles/storymodal';
 import assetPaths from '../assetPaths';
-import storyContent from '../../data/stories/stories';
-
-
 
 class StoryModalScreen extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			friendRequested: false,
-			heartSolid: false,
-			starSolid: false,
-
-		};
-		//console.log("story modal screen props", this.props);
-
-
+		this.state = { friendRequested: false, heartSolid: false, starSolid: false, };
 	}
 
 	addFriend() {
-
 		if (this.state.friendRequested) {
-			this.setState({
-				friendRequested: false,
-			});
+			this.setState({ friendRequested: false });
 		} else {
-			this.setState({
-				friendRequested: true,
-			});
+			this.setState({ friendRequested: true });
 		}
-
 	}
 
 	renderStoryPng(id) {
-
 		switch (id) {
 			case 1:
 				return (<Image style={{width: width, height: 2719, marginBottom: 100, top: -84}} source={require('../../assets/staticScreens/stories/marleyRevised2x.png')} />);
@@ -71,54 +50,37 @@ class StoryModalScreen extends React.Component {
 	}
 
   render() {
-
-		const {params} = this.props.navigation.state;
-		const id = params ? params.id: 1;
-		console.log("rendering id", id);
-
-    return (
+	const {params} = this.props.navigation.state;
+	const id = params ? params.id: 1;
+	return (
 		<View style={s.modalContainer}>
-			
 			<View style={[s.modalMenuContainer, {width: width}]}>
 				<TouchableOpacity style={s.modalButton} >
 					<Image style={s.shareIcon} source={assetPaths.modals.shareModal.share} />
 				</TouchableOpacity>
 				<TouchableOpacity style={s.modalButton} onPress={() => this.setState({starSolid: !this.state.starSolid})}>
-					
 					{ this.state.starSolid ? 
-						<Image style={s.starIcon} source={assetPaths.modals.shareModal.starSolid} /> : 
-						<Image style={s.starIcon} source={assetPaths.modals.shareModal.star} />
-					}
-					
+					<Image style={s.starIcon} source={assetPaths.modals.shareModal.starSolid} /> : 
+					<Image style={s.starIcon} source={assetPaths.modals.shareModal.star} /> }
 				</TouchableOpacity>
 				<TouchableOpacity style={s.modalButton} onPress={() => this.setState({heartSolid: !this.state.heartSolid})}>
-
 					{ this.state.heartSolid ? 
 						<Image style={s.heartIcon} source={assetPaths.modals.shareModal.likeSolid} /> :
 						<Image style={s.heartIcon} source={assetPaths.modals.shareModal.like} />
 					}
-					
 				</TouchableOpacity>
-			
 				<TouchableOpacity style={s.addFriend} onPress={() => (this.addFriend())} >
 					<Image style={s.addFriendImage} source={
 						this.state.friendRequested ? assetPaths.modals.shareModal.requestSent :
-						assetPaths.modals.shareModal.addFriend
-
-						}/>
+						assetPaths.modals.shareModal.addFriend }/>
 				</TouchableOpacity>
 				<TouchableOpacity style={s.exitButton} onPress={() => this.props.navigation.goBack()} >
 					<Image style={s.exitIconImage} source={assetPaths.modals.shareModal.close}/>
 				</TouchableOpacity>
-				
-		</View>
-  		<ScrollView style={s.storyContentContainer}>
-  			{ this.renderStoryPng(id)}
-		</ScrollView>
-
-
-</View>);
+			</View>
+  			<ScrollView style={s.storyContentContainer}> { this.renderStoryPng(id)} </ScrollView>
+		</View>);
   }
 }
 
-export default StoryModalScreen;
+export default StoryModalScreen
